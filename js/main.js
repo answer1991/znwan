088064002731722a64d4e22347b7088fc45b9530
陈俊 <answer1991.chen@gmail.com>
2013-05-11 19:54:04 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zet
+ * Date: 13-5-11
+ * Time: 下午3:02
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+(function(win, doc){
+
+})(window, document);
